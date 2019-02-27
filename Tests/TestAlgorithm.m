f132394e6a8056cc58bce17eec19969f17137d1c
Andrew Savino <andrewsavino1@gmail.com>
2019-02-26 22:10:35 -0600

    hotfix to correct for errors caused by adding arguments to functions

--- Tests/TestAlgorithm.m
+++ Tests/TestAlgorithm.m
@@ -14,7 +14,7 @@ function testMajorChord(testCase)
     low_index = 1;
     high_index = 88;
     [ET_notes, ET_tolerance_bands] = generateETNotes(low_index,high_index);
-    outputFreqs = algorithm(majorChord, ET_notes, ET_tolerance_bands);
+    outputFreqs = algorithm(majorChord, ET_notes, ET_tolerance_bands, 0);
     
     verifyEqual(testCase, outputFreqs(2)/5, outputFreqs(1)/4,...
         'Third of major chord is incorrect')
@@ -30,12 +30,28 @@ function testMinorChord(testCase)
     high_index = 88;
 
     [ET_notes, ET_tolerance_bands] = generateETNotes(low_index,high_index);
-    outputFreqs = algorithm(majorChord, ET_notes, ET_tolerance_bands);
+    outputFreqs = algorithm(majorChord, ET_notes, ET_tolerance_bands, 0);
     
     verifyEqual(testCase, outputFreqs(2)/6, outputFreqs(1)/5,...
         'Third of major chord is incorrect')
     verifyEqual(testCase, outputFreqs(3)/6, outputFreqs(1)/4,...
         'Fifth of major chord is incorrect')
+end
+
+
+function testNoteDoesntMoveTooFar(testCase)
+    algorithm = @Algorithm1;
+	basenote = n('C', 4);
+    low_index_1 = 1;
+    low_index_2 = 7;
+    high_index = 88;
+    [ET_notes1, ET_tolerance_bands1] = generateETNotes(low_index_1,high_index);
+    [ET_notes2, ET_tolerance_bands2] = generateETNotes(low_index_2,high_index);
+    outputFreqs1 = algorithm(basenote, ET_notes1, ET_tolerance_bands1, 0);
+    outputFreqs2 = algorithm(basenote, ET_notes2, ET_tolerance_bands2, 6);
+    
+    verifyEqual(testCase, outputFreqs1, outputFreqs2,...
+        'When changing the starting point of the scale, the chord changed transposition')
 end
 
 
