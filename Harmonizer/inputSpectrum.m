d1caa608e19fbf0076356a02baaba752f8111301
AndrewSenol <andrewaes77@gmail.com>
2019-04-23 23:26:13 -0500

    created equal temperament and just intonation chords

--- Harmonizer/inputSpectrum.m
+++ Harmonizer/inputSpectrum.m
@@ -1,31 +1,38 @@
 % Method for analyzing freq spectrum of input audio signal
 % Choose FFT size and calculate spectrum
-Nfft = 1024;
+Nfft = 4096;
 fsamp = 44100;
 Tsamp = 1/fsamp;
 
-audioFile = 'three_note_progression.wav';
+audioFile = 'SENOL_SAVINO_ET.wav';
+audioFile2 = 'SENOL_SAVINO_JI.wav';
 
 [input_signal, fs] = audioread(audioFile);
+[input_signal2, fs2] = audioread(audioFile2);
 
 t1 = 1:length(input_signal);
 
 subplot(2,1,1)
-spectrogram(input_signal(:,1),gausswin(Nfft),Nfft/2,Nfft,fsamp, 'yaxis');
-title('Singing A4 with "Ah" vowel');
+new_signal = input_signal2(:,1) - input_signal(:,1);
+spectrogram(input_signal(:,1),gausswin(Nfft),Nfft/16,Nfft,fsamp, 'yaxis');
+title('Equal Temperament');
 set(gca, 'yscale','log');
+ylim([0.2 5]);
 
 
 % Plot frequency spectrum
 subplot(2,1,2);
-[Pxx,f] = pwelch(input_signal,gausswin(Nfft),Nfft/2,Nfft,fsamp);
+spectrogram(input_signal2(:,1),gausswin(Nfft),Nfft/16,Nfft,fsamp, 'yaxis');
+set(gca, 'yscale','log');
+ylim([0.2 5]);
+%[Pxx,f] = pwelch(input_signal,gausswin(Nfft),Nfft/2,Nfft,fsamp);
 
 
-loglog(f,Pxx);
-ylabel('PSD'); 
-xlabel('Frequency (Hz)');
-axis auto;
-grid on;
+%loglog(f,Pxx);
+% ylabel('PSD'); 
+% xlabel('Frequency (Hz)');
+% axis auto;
+% grid on;
 
 % Get frequency estimate (spectral peak)
 [~,loc] = max(Pxx);
@@ -34,14 +41,14 @@ estimate = f(loc);
 FREQ_ESTIMATE_gaussian = estimate(1,:);
 
 
-% title('Periodogram Power Spectral Density Estimate');
-title(['PSD Frequency estimate = ',num2str(FREQ_ESTIMATE_gaussian(1)),' Hz']);
+title('Just Intonation');
+%title(['PSD Frequency estimate = ',num2str(FREQ_ESTIMATE_gaussian(1)),' Hz']);
 
 
-sound(input_signal, 44100)
-pause(length(input_signal)/44100)
-sound(input_signal, (3/2)*44100)
-% %sound(input_signal, (5/4)*44100)
-sound(input_signal, (6/5)*44100)
-% %sound(input_signal, (7/4)*44100)
-sound(input_signal, (8/5)*44100)
+% soundsc(input_signal, 44100)
+% %pause(length(input_signal)/44100)
+% sound(input_signal, (3/2)*44100)
+% % %sound(input_signal, (5/4)*44100)
+% sound(input_signal, (6/5)*44100)
+% % %sound(input_signal, (7/4)*44100)
+% sound(input_signal, (8/5)*44100)
